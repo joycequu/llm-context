f1895ded3c5963cd2a2f241eb59feb81e2bb73ed
Joyce Qu <joycequ2016@gmail.com>
2024-08-08 13:25:44 -0400

    debug pt3

--- download_model.py
+++ download_model.py
@@ -1,9 +1,20 @@
 # from huggingface_hub import login
 # # login(token='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')
 # login(token='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')
-
 import os
-from transformers import AutoTokenizer, AutoModel
+import tempfile
+from pathlib import Path
+from filelock import SoftFileLock
+from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
+import huggingface_hub
+from huggingface_hub import file_download, _local_folder
+
+def use_softfilelock_in_hf():
+    huggingface_hub.utils.WeakFileLock = SoftFileLock
+    file_download.WeakFileLock = SoftFileLock
+    _local_folder.WeakFileLock = SoftFileLock
+
+use_softfilelock_in_hf()
 
 # Ensure your Hugging Face token is set as an environment variable
 token = os.getenv('HUGGINGFACE_TOKEN')
