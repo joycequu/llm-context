4442a75f92a80b500598f882692c220b8602386f
Joyce Qu <joycequ2016@gmail.com>
2024-08-08 13:22:06 -0400

    debug

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 export HDF5_USE_FILE_LOCKING='FALSE'
 export HF_HOME=/state/partition1/user/$(whoami)/hf
 mkdir -p $(HF_HOME)
+export HF_DATASETS_CACHE=/home/gridsan/ywang5/hf/datasets
+export TRANSFORMERS_CACHE=/home/gridsan/ywang5/hf/models
 
 python download_model.py
