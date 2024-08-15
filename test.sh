40d5baaad019e9887fb5935577ef6db1e9b83452
Joyce Qu <joycequ2016@gmail.com>
2024-08-15 08:55:47 -0400

    batch for jcb

--- test.sh
+++ test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # Loading the required module
 module load anaconda/2023a
-
-export TMPDIR=/state/partition1/user/$USER
-mkdir $TMPDIR
-export TRANSFORMERS_CACHE=/home/gridsan/ywang5/hf/models
-export HF_HOME=/state/partition1/user/$(whoami)/hf
-# mkdir -p "$HF_HOME"
-
 python /home/gridsan/ywang5/projects/llm-context/long_context.py --model_path "/home/gridsan/ywang5/hf/models/vicuna-7b-v1.3"
