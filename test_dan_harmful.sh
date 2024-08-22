61408b8945b024529ceebe4af761607ec667c1c5
Joyce Qu <joycequ2016@gmail.com>
2024-08-21 22:25:29 -0500

    attempt short_dan

---
+++ test_dan_harmful.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Loading the required module
+module load anaconda/2023a
+
+export HF_HOME=/home/gridsan/ywang5/hf/misc
+export HF_DATASETS_CACHE=/home/gridsan/ywang5/hf/datasets
+export TRANSFORMERS_CACHE=/home/gridsan/ywang5/hf/models
+export TMPDIR=/state/partition1/user/$USER
+mkdir $TMPDIR
+
+python /home/gridsan/ywang5/projects/llm-context/long_context_dan.py --model_path "/home/gridsan/ywang5/hf/models/vicuna-7b-v1.3" --attack "short_dan"
