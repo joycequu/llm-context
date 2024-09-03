1d62e4304f4fee676562a0d83f5fb12ba485b565
Joyce Qu <joycequ2016@gmail.com>
2024-09-03 15:35:47 -0400

    user eval

--- evaluate_benign_completions_manual.py
+++ evaluate_benign_completions_manual.py
@@ -6,8 +6,9 @@ from collections import defaultdict
 
 YELLOW = '\033[93m'
 RESET = '\033[0m'
+RED = '\033[91m'
 
-completion_path = "answer_mistral_general_dan_news_benign.json"
+completion_path = "completions/answer_mistral_general_dan_news_benign.json"
 output_path = "results/mistral_general_dan_news_benign_accuracy.csv"
 # Open the JSON file
 with open(completion_path, 'r') as file:
@@ -24,11 +25,15 @@ def add_to_csv(context_length, category_accuracy, if_index=False):
     all_entries = 0
     all_accurate = 0
     for category, accuracy in category_accuracy.items():
+        print(f'{accuracy=}')
         entry["category"].append(category)
         entry["context_length"].append(context_length)
         entry["num_accurate"].append(accuracy[0])
         entry["num_total"].append(accuracy[1])
-        entry["accuracy"].append(accuracy[0]/accuracy[1])
+        if accuracy[1] != 0:
+            entry["accuracy"].append(accuracy[0]/accuracy[1])
+        else:
+            entry["accuracy"].append(0)
         all_accurate += accuracy[0]
         all_entries += accuracy[1]
     # Add total for this context length
@@ -36,15 +41,18 @@ def add_to_csv(context_length, category_accuracy, if_index=False):
     entry["context_length"].append(context_length)
     entry["num_accurate"].append(all_accurate)
     entry["num_total"].append(all_entries)
-    entry["accuracy"].append(all_accurate/all_entries)
+    if all_entries != 0:
+        entry["accuracy"].append(all_accurate/all_entries)
+    else:
+        entry["accuracy"].append(0)
     # write to csv
     df_new = pd.DataFrame(entry)
     df_existing = pd.read_csv(output_path)
     df_combined = pd.concat([df_existing, df_new], ignore_index=if_index)
     df_combined.to_csv(output_path, header=True, index=if_index)
-    print("Wrote to CSV")
+    print(f"{RED}Wrote to CSV{RESET}")
 
-data = data[:200] # trial - try first 200
+data = data[:240] # trial - try first 200
 
 # Process each item one by one
 # Every 100 items is one repeat of a specific context_length
@@ -55,6 +63,7 @@ for item in data:
     print("############ NEW QUESION ###################")
     tally += 1
     # Read in / Display information
+    print(f'question_id={item["question_id"]}')
     print(f'{YELLOW}GOAL={item["goal"]}{RESET}')
     print(f'ANSWER={item["answer"]}')
     context_length = item['context_length']
@@ -62,17 +71,22 @@ for item in data:
     print(f'{context_length=}')
 
     # Ask for manual evaluation
-    if_accurate = int(input(f"Is answer accurate (1-Yes, 0-No)? : "))
+    user_eval = input(f"Is answer accurate (1-Yes, 0-No)? : ")
+    if user_eval == "":
+        if_accurate = 1
+    else:
+        if_accurate = 0
     item['accuracy'] = if_accurate
 
+    category_accuracy[category][0] += if_accurate
+    category_accuracy[category][1] += 1
+    
     # Update category accuracy
-    if tally == 100:
-        category_accuracy = defaultdict(lambda: [0, 0])
+    if tally == 80:
         add_to_csv(context_length, category_accuracy)
         print("New repeat for {context_length=}")
         tally = 0 # clear tally for next repeat
-    category_accuracy[category][0] += if_accurate
-    category_accuracy[category][1] += 1
+        category_accuracy = defaultdict(lambda: [0, 0])
 
 # Write the updated data back to the same file
 with open(completion_path, 'w') as file:
