a4ed4c24b55b9089cdf82a760373d0df885e8d43
Joyce Qu <joycequ2016@gmail.com>
2024-09-03 09:11:28 -0400

    test imports

--- test_imports.py
+++ test_imports.py
@@ -1,13 +1,12 @@
-import argparse
-import torch
-from tqdm import tqdm
-from fastchat.model import load_model, get_conversation_template, add_model_args
-from transformers import AutoTokenizer, AutoModel, AutoConfig, AutoModelForCausalLM
-from config import *
-import pandas as pd
-from pathlib import Path
-from filelock import SoftFileLock
-import huggingface_hub
-from huggingface_hub import file_download, _local_folder
+import transformers
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
 import json
-import os
+import argparse
+import os, logging
+import csv
+from tqdm import tqdm 
+import numpy as np
+import logging
+
+from vllm import LLM, SamplingParams
