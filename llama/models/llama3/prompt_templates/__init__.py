f7a572de6deee6b422dec3ba55f4afdf771a9f4d
Joyce Qu <joycequ2016@gmail.com>
2024-09-08 15:39:26 -0400

    remove llama3

--- llama/models/llama3/prompt_templates/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import PromptTemplate, PromptTemplateGeneratorBase
-from .system_prompts import (
-    BuiltinToolGenerator,
-    FunctionTagCustomToolGenerator,
-    JsonCustomToolGenerator,
-    SystemDefaultGenerator,
-)
-from .tool_response import ToolResponseGenerator
